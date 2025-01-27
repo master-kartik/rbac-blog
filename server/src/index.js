2748df35040e3db9ea2f11ca6b45c571b9db088b
Kartik Kochhar <kochharkartik51@gmail.com>
2025-01-27 19:10:46 +0530

    update

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ app.use(cp());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", blogRoutes);
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
